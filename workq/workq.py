5b5f10fb895e94b48a6edb6612a5d63db8e310a6
Hideo Hattori <hattori-h@klab.com>
2016-10-26 11:35:57 +0900

    split send method

--- workq/workq.py
+++ workq/workq.py
@@ -53,23 +53,31 @@ class WorkqClient(object):
         self._timeout = 3
 
     @asyncio.coroutine
+    def send(self, msg, timeout):
+        self._w.write(str.encode(msg))
+        future = self._w.drain()
+        yield from asyncio.wait_for(future, timeout=timeout/1000., loop=self.loop)
+
+    @asyncio.coroutine
     def connect(self):
         future = asyncio.open_connection(self.host, self.port, loop=self.loop)
         self._r, self._w = yield from asyncio.wait_for(future, timeout=self._timeout, loop=self.loop)
 
     @asyncio.coroutine
     def lease(self, names, timeout=60000):
+        # build request message for lease command
         proto_names = " ".join(names)
         msg = "lease %s %d\r\n" % (proto_names, timeout)
-        # send request
-        self._w.write(str.encode(msg))
-        future = self._w.drain()
-        yield from asyncio.wait_for(future, timeout=timeout/1000., loop=self.loop)
 
-        # wait response
+        # send request
+        yield from self.send(msg, self._timeout)
+
+        # recv response
         future = self._r.readline()
-        buf = yield from asyncio.wait_for(future, timeout=5, loop=self.loop)
+        buf = yield from asyncio.wait_for(future, timeout=timeout*1.3/1000., loop=self.loop)
         rnum = WorkqProtocol.check_response(buf)
+
+        # recv response body
         jobs = []
         for i in range(rnum):
             future = self._r.readline()
@@ -81,36 +89,14 @@ class WorkqClient(object):
 
     @asyncio.coroutine
     def add_job(self, job):
-        # build request message for workq protocol
-        options = []
-        if job.priority != 0:
-            options.append("-priority=%d" % job.priority)
-        if job.max_attempts != 0:
-            options.append("-max-attempts=%d" % job.max_attempts)
-        if job.max_fails != 0:
-            options.append("-max-fails=%d" % job.max_fails)
-        option_arg = "" if len(options) == 0 else " " + " ".join(options)
-        msg = "add %s %s %d %d %d%s\r\n%s\r\n" % (
-                job.id, job.name, job.ttr, job.ttl, len(job.payload), option_arg, job.payload)
+        """add background job"""
+        # build request message for add command
+        msg = job.to_proto()
 
         # send request
-        self._w.write(str.encode(msg))
-        future = self._w.drain()
-        yield from asyncio.wait_for(future, timeout=self._timeout, loop=self.loop)
+        yield from self.send(msg, self._timeout)
 
         # read reply
         future = self._r.readline()
         buf = yield from asyncio.wait_for(future, timeout=self._timeout, loop=self.loop)
         WorkqProtocol.check_response(buf)
-
-
-if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    client = WorkqClient('127.0.0.1', 9922, loop)
-    job = BackgroundJob(uuid.uuid4(), "ping1", 5000, 60000, "hello bg job")
-    try:
-        loop.run_until_complete(client.connect())
-        loop.run_until_complete(client.add_job(job))
-        loop.run_until_complete(client.lease())
-    finally:
-        loop.close()
