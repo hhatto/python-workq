723742acedf7cba511d6bf334a64e170be9e8187
Hideo Hattori <hattori-h@klab.com>
2016-10-26 11:35:24 +0900

    add ForegroundJob class

--- workq/job.py
+++ workq/job.py
@@ -10,6 +10,30 @@ class BackgroundJob(object):
         self.max_attempts = max_attempts
         self.max_fails = max_fails
 
+    def to_proto(self):
+        options = []
+        if self.priority != 0:
+            options.append("-priority=%d" % self.priority)
+        if self.max_attempts != 0:
+            options.append("-max-attempts=%d" % self.max_attempts)
+        if self.max_fails != 0:
+            options.append("-max-fails=%d" % self.max_fails)
+        option_arg = "" if len(options) == 0 else " " + " ".join(options)
+        msg = "add %s %s %d %d %d%s\r\n%s\r\n" % (
+                self.id, self.name, self.ttr, self.ttl, len(self.payload), option_arg, self.payload)
+        return msg
+
+
+class ForegroundJob(object):
+
+    def __init__(self, jobid, name, ttr, timeout, payload, priority=0):
+        self.id = jobid
+        self.name = name
+        self.ttr = ttr
+        self.timeout = timeout
+        self.payload = payload
+        self.priority = priority
+
 
 class LeasedJob(object):
 
