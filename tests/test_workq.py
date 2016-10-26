756bb5810702479c2fb3b2cb34a7984176a50d9f
Hideo Hattori <hattori-h@klab.com>
2016-10-26 11:36:35 +0900

    add test for leased job values and lease timeout

--- tests/test_workq.py
+++ tests/test_workq.py
@@ -2,6 +2,7 @@ import unittest
 import uuid
 import asyncio
 from workq.workq import WorkqClient
+from workq.error import WorkqTimeout
 from workq.job import BackgroundJob
 
 
@@ -10,12 +11,26 @@ class TestWorkq(unittest.TestCase):
     def test_smoke(self):
         loop = asyncio.new_event_loop()
         client = WorkqClient('127.0.0.1', 9922, loop)
-        job = BackgroundJob(uuid.uuid4(), "ping1", 5000, 60000, "hello bg job")
+        jobid = uuid.uuid4()
+        job = BackgroundJob(jobid, "ping1", 5000, 60000, "hello bg job")
         try:
             loop.run_until_complete(client.connect())
             loop.run_until_complete(client.add_job(job))
             leased_job = loop.run_until_complete(client.lease(("ping1", ), 10000))
-            print(leased_job)
+            self.assertEqual(len(leased_job), 1)
+            self.assertEqual(leased_job[0].id, "%s" % jobid)
+            self.assertEqual(leased_job[0].name, "ping1")
+        finally:
+            loop.close()
+
+    def test_lease_timeout(self):
+        loop = asyncio.new_event_loop()
+        client = WorkqClient('127.0.0.1', 9922, loop)
+        try:
+            loop.run_until_complete(client.connect())
+            with self.assertRaises(WorkqTimeout):
+                leased_job = loop.run_until_complete(client.lease(("ping1", ), 100))
+                print(leased_job)
         finally:
             loop.close()
 
